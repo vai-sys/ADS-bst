23468f9a71e7a9844ed243d7df73ff8eb9ac10bf
vaishnavi Thorat <122679661+vai-sys@users.noreply.github.com>
2023-12-03 23:09:26 +0530

    Add files via upload

---
+++ bst.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int key;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int key) {
+    struct TreeNode* newNode = malloc(sizeof(struct TreeNode));
+    newNode->key = key;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct TreeNode* insert(struct TreeNode* root, int key) {
+    if (root == NULL) {
+        return createNode(key);
+    }
+
+    if (key < root->key) {
+        root->left = insert(root->left, key);
+    } else if (key > root->key) {
+        root->right = insert(root->right, key);
+    }
+
+    return root;
+}
+
+void inorderTraversal(struct TreeNode* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->key);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("In-order Traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    // Note: In a real-world scenario, you should free allocated memory.
+    return 0;
+}
